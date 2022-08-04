aafb9dbe868a70581c2438240ae87c14a913eac8
Igor <vashkevichigor55@gmail.com>
2022-08-04 16:05:40 +0300

    Я поработал с классом String реализовал методы equals,concat, countSymbols.

--- src/main/java/stringLessons/ConcatString.java
+++ src/main/java/stringLessons/ConcatString.java
@@ -1,48 +1,52 @@
 package stringLessons;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+
 
 public class ConcatString {
     public static void main(String[] args) throws NullStringInputException{
         ConcatString concatString = new ConcatString();
-        concatString.equals("Igor", "Igor");
-        concatString.countSymbols("");
+
 
 
     }
 
     public void concat(String str1, String str2) {
 
-        if (str1 == null || str1.isEmpty())
-            throw new NullStringInputException("Нельзя скелить т.к первая строка пустая");
-        if (str2 == null  || str2.isEmpty())
-            throw new NullStringInputException("Нельзя скелить т.к вторая строка пустая");
-        else
+            checkEmptyOrNullCase(str1,str2);
             System.out.println(str1 + " " + str2);
-    }
-
-    public void equals(String str1, String str2) {
-        if (str1 == null || str1.isEmpty())
-            throw new NullStringInputException("Первая строка пустая");
-        if (str2 == null  || str2.isEmpty())
-            throw new NullStringInputException("Вторая строка пустая");
-        if (str1.equals(str2))
-            System.out.println("Строки равны");
-        else {
-            Date date = new Date();
-            System.out.println(date);
-        }
     }
 
     public void countSymbols(String str1) {
         int count = 0;
-        str1 = str1.replace(" ", "");
-        if (str1.isEmpty())
+        if (str1 == null || str1.isEmpty()) {
             throw new NullStringInputException("Строка пустая");
+        }
+        str1 = str1.replace(" ", "");
+
+
 
         for (int i = 0; i < str1.length(); i++) {
             count++;
         }
         System.out.println(count);
+    }
+
+    public void equals(String str1, String str2) {
+        checkEmptyOrNullCase(str1,str2);
+        if (str1.equals(str2)){
+            System.out.println("Строки равны");
+        } else {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        }
+
+    }
+
+    private void checkEmptyOrNullCase(String str1, String str2) {
+        if (str1 == null || str1.isEmpty()){
+            throw new NullStringInputException("Первая строка пустая");}
+        if (str2 == null  || str2.isEmpty()){
+            throw new NullStringInputException("Вторая строка пустая");}
     }
 }
