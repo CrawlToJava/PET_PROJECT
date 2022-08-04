28c15055493d4a16946bd6914549e22d2adb96a8
Igor <vashkevichigor55@gmail.com>
2022-08-04 16:53:15 +0300

    Я поработал с классом String реализовал методы equals,concat, countSymbols.

--- src/main/java/stringLessons/ConcatString.java
+++ src/main/java/stringLessons/ConcatString.java
@@ -4,43 +4,32 @@ import java.time.LocalDateTime;
 
 
 public class ConcatString {
-    public static void main(String[] args) throws NullStringInputException{
-        ConcatString concatString = new ConcatString();
-
-
-
-    }
-
-    public void concat(String str1, String str2) {
-
+    public String concat(String str1, String str2) {
             checkEmptyOrNullCase(str1,str2);
-            System.out.println(str1 + " " + str2);
+            return str1 + " " + str2;
     }
 
-    public void countSymbols(String str1) {
+    public int countSymbols(String str1) {
         int count = 0;
         if (str1 == null || str1.isEmpty()) {
             throw new NullStringInputException("Строка пустая");
         }
         str1 = str1.replace(" ", "");
-
-
-
         for (int i = 0; i < str1.length(); i++) {
             count++;
         }
-        System.out.println(count);
+        return count;
     }
 
-    public void equals(String str1, String str2) {
+    public boolean equals(String str1, String str2) {
         checkEmptyOrNullCase(str1,str2);
         if (str1.equals(str2)){
-            System.out.println("Строки равны");
+            return true;
         } else {
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
+        return false;
         }
-
     }
 
     private void checkEmptyOrNullCase(String str1, String str2) {
